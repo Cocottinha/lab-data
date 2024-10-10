1b0068be15233b44713612adf43838646612e5fd
Lucas Cotta Muniz <lucascottamuniz@hotmail.com>
2024-10-10 15:15:05 -0300

    att

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,22 @@ const routes = [
     component: () => import('../views/PostView.vue')
   },
   {
+    path: '/grafico/:id',
+    name: 'Grafico',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/GraficoView.vue')
+  },
+  {
+    path: '/imagem/:id',
+    name: 'Image',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/ImagemView.vue')
+  },
+  {
     path: '/login',
     name: 'Login',
     // route level code-splitting
